b7149eebcd114e314e93d6b5a6ee400b670b7fa2
Sohel Rana <89797602+sohelrana3@users.noreply.github.com>
2025-02-25 11:04:03 +0600

    fontend

--- fontend/src/components/Products.jsx
+++ fontend/src/components/Products.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Products = () => {
+  let [data, setdata] = useState([]);
+  // useEffect create
+  useEffect(() => {
+    async function allData() {
+      const data = await axios.get("http://localhost:8000/api/allProduct");
+      setdata(data.data);
+    }
+    allData();
+  });
   return (
     <div className="contianer max-w-6xl mx-auto my-3 ">
       <div className=" mx-auto text-center">
@@ -12,23 +22,28 @@ const Products = () => {
       </div>
 
       {/* Products section */}
-      <div className="grid grid-cols-4 pb-4 ">
+      <div className="grid grid-cols-4 gap-x-3 pb-4 ">
         {/* map */}
-        <div className="flex flex-col items-center gap-x-3 border border-gray-200 rounded-xl hover:outline-dashed hover:border-none outline-gray-200 pb-4">
-          <img
-            src="https://playgrow.qodeinteractive.com/wp-content/uploads/2022/11/Shop-Single-gallery-img-05.jpg"
-            alt=""
-          />
 
-          <a href="" className="text-gray-600">BEDS</a>
+        {data.map((item, index) => (
+          <div key={index} className="flex flex-col items-center gap-x-3 border border-gray-200 rounded-xl hover:outline-dashed hover:border-none outline-gray-200 pb-4">
+            <img
+              src={item.image}
+              alt=""
+            />
 
-          <h5 className="text-2xl">
-            <a href="">Premium Bed Wooden</a>
-          </h5>
-          <div>
-            <span className="text-gray-600 ">$300.00</span>
+            <a href="" className="text-gray-600">
+            {item.name}
+            </a>
+
+            <h5 className="text-2xl">
+              <a href="">{item.title}</a>
+            </h5>
+            <div>
+              <span className="text-gray-600 ">${item.price}.00</span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
